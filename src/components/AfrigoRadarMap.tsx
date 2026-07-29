706b97dd0e23a294b51f7243547bb1843c7cc1c1
jhskmj7-wq <jhs.kmj7@gmail.com>
2026-07-29 10:39:57 +0000

    fix(map): remove retina suffix from map tile URLs
    Update CartoDB tile URLs to resolve rendering issues caused by the invalid {r} suffix.

--- src/components/AfrigoRadarMap.tsx
+++ src/components/AfrigoRadarMap.tsx
@@ -103,9 +103,9 @@ export const AfrigoRadarMap: React.FC<AfrigoRadarMapProps> = ({
             "osm-tiles": {
               type: "raster",
               tiles: [
-                "https://a.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
-                "https://b.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
-                "https://c.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+                "https://a.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png",
+                "https://b.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png",
+                "https://c.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png"
               ],
               tileSize: 256,
               attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
